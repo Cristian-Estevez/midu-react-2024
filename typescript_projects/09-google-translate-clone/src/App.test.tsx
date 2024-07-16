7c6a2706c7ba51d47dc1c66b1cf917474ef15d3a
Cristian Estevez <cuentacursosprogramacion@gmail.com>
2024-07-16 18:52:41 -0300

    Se agrega librería de testing y primer test

---
+++ typescript_projects/09-google-translate-clone/src/App.test.tsx
@@ -0,0 +1,21 @@
+import { describe, it, expect, test } from "vitest";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import App from "./App";
+
+test("App works as expected", async () => {
+  const app = render(<App />);
+
+  const user = userEvent.setup();
+
+  const textareaFrom = app.getByPlaceholderText("Introducir texto");
+  await user.type(textareaFrom, "Hola mundo");
+
+  const result = await app.findByDisplayValue(
+    /Hello world/i,
+    {},
+    { timeout: 2000 },
+  );
+  expect(result).toBeTruthy;
+});
